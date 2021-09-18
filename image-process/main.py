019605252693bd968e2b8e665246013e6f9b6a91
huitae111 <huitae5575@naver.com>
2021-09-19 02:45:58 +0900

    이미지 특성 매칭

--- image-process/main.py
+++ image-process/main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import os 
 try: 
     from PIL import Image 
@@ -10,6 +11,24 @@ capture = cv2.VideoCapture(0)                        #카메라 설정
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 600)           
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
 
+def featureMatching():
+    img1 = cv2.imread('기준이될 싸인 주소', cv2.IMREAD_GRAYSCALE)
+    img2 = cv2.imread('대조할 싸인 주소', cv2.IMREAD_GRAYSCALE)
+    res = None
+
+    orb = cv2.ORB_create()
+    kp1, des1 =orb.detectAndCompute(img1, None)
+    kp2, des2 =orb.detectAndCompute(img2, None)
+
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = bf.match(des1, des2)
+
+    matches = sorted(matches, key=lambda x:x.distance)
+    res = cv2.drawMatches(img1, kp1, img2, kp2, matches[:30], res, flags=0)
+
+    cv2.inshow('Feature Mathhing', res)
+
+featureMatching()
 # gray = cv2.cvtColor(capture, cv2.COLOR_RGB2GRAY)         #윤곽선 검출
 # ret, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
 # binary = cv2.bitwise_not(binary)
@@ -37,3 +56,5 @@ print(text)
 
 cv2.imshow("capture", capture)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
+
