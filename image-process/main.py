ee20c6f4358e2775250a114bb5deca53833d5021
huitae111 <huitae5575@naver.com>
2021-09-22 16:43:47 +0900

    윤곽선

--- image-process/main.py
+++ image-process/main.py
@@ -1,60 +1,28 @@
 import cv2
-import numpy as np
-import os 
-try: 
-    from PIL import Image 
-except ImportError: 
-    import Image 
-import pytesseract
+import os
+import os.path
 
-capture = cv2.VideoCapture(0)                        #카메라 설정
-capture.set(cv2.CAP_PROP_FRAME_WIDTH, 600)           
-capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
+path = './img/싸인.png'
+src = cv2.imread(path)
 
-def featureMatching():
-    img1 = cv2.imread('기준이될 싸인 주소', cv2.IMREAD_GRAYSCALE)
-    img2 = cv2.imread('대조할 싸인 주소', cv2.IMREAD_GRAYSCALE)
-    res = None
+if os.path.exists(path):
+    print(src)
 
-    orb = cv2.ORB_create()
-    kp1, des1 =orb.detectAndCompute(img1, None)
-    kp2, des2 =orb.detectAndCompute(img2, None)
+gray = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
+tree = src.copy()
 
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    matches = bf.match(des1, des2)
+contours, hierarchy = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+for contour in contours:
+    src = cv2.drawContours(src, [contour], -1, (0, 255, 0), 2)
 
-    matches = sorted(matches, key=lambda x:x.distance)
-    res = cv2.drawMatches(img1, kp1, img2, kp2, matches[:30], res, flags=0)
+contour,hierarchy = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+for contour in contours:
+    tree = cv2.drawContours(tree, [contour], -1, (0, 255, 0), 2)
 
-    cv2.inshow('Feature Mathhing', res)
 
-featureMatching()
-# gray = cv2.cvtColor(capture, cv2.COLOR_RGB2GRAY)         #윤곽선 검출
-# ret, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-# binary = cv2.bitwise_not(binary)
+cv2.imshow('ext', src)
+cv2.imshow('tree', tree)
 
-# contours, hierarchy = cv2.findContours(binary, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
 
-# for i in range(len(contours)):
-#     cv2.drawContours(capture, [contours[i]], 0, (0, 0, 255), 2)
-#     cv2.putText(capture, str(i), tuple(contours[i][0][0]), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0), 1)
-#     print(i, hierarchy[0][i])
-#     cv2.imshow("capture", capture)
-#     cv2.waitKey(0)
-
-# 설치한 tesseract 프로그램 경로 (64비트) 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract' 
-# 이미지 불러오기, Gray 프로세싱 
-gray = cv2.cvtColor(capture, cv2.COLOR_BGR2GRAY)
-filename = "{}.png".format(os.getpid())
-cv2.imwrite(filename, gray)
- # Simple image to string 
-text = pytesseract.image_to_string(Image.open(filename), lang=None)
-os.remove(filename)
-
-print(text)
-
-cv2.imshow("capture", capture)
-cv2.waitKey(0)
+cv2.waitKey()
 cv2.destroyAllWindows()
-
